1a2601f332214d77cb0860ed51804a6b02479a46
Shwethar <64232154+shwethar1@users.noreply.github.com>
2021-06-26 23:46:27 -0400

    developed and finished entire react app on local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
+import Feed from './components/Feed'
+import LoginScreen from './components/LoginScreen';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
-export default App;
+    if (!localStorage.getItem('username')) return <LoginScreen />;
+
+    return (
+
+        <ChatEngine
+
+        height="100vh" //the chat application takes up most of the app so we can set it to 100
+        projectID="9ae763f9-c429-44ae-869c-a952e4b31937" //chat engine ran by chatengine.io- Called Shwetha's ReactChat App
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps)=> <Feed {...chatAppProps} /> }
+        // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
+      );
+    };
+    
+    
+    export default App;
